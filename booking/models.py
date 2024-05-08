ae47c59d2488ad61f9e4e44c0b3926ca30227c6c
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-08 12:35:54 +0000

    fix: Validate RestaurantTable values

--- booking/models.py
+++ booking/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class RestaurantTable(models.Model):
-    table_number = models.IntegerField(unique=True)
-    capacity = models.IntegerField()
+    table_number = models.PositiveIntegerField(unique=True)
+    capacity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
