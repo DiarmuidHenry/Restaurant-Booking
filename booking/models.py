5949b2a45af7b77234a79337607771fc48e3169f
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-08 12:10:02 +0000

    feat RestaurantTable model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class RestaurantTable(models.Model):
+    table_number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
