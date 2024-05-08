2eddd96c5fc2e526496fddf6a619ba675d8548f0
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-08 12:59:45 +0000

    feat: Reservation model

--- booking/models.py
+++ booking/models.py
@@ -7,3 +7,16 @@ from django.contrib.auth.models import User
 class RestaurantTable(models.Model):
     table_number = models.PositiveIntegerField(unique=True)
     capacity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+class Reservation(models.Model):
+    STATUS_CHOICES = [
+        (1, 'Confirmed'),
+        (0, 'Canceled'),
+    ]
+
+    reservation_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=100)
+    table = models.ForeignKey(RestaurantTable, on_delete=models.CASCADE)
+    date_and_time = models.DateTimeField(default=timezone.now)
+    number_of_guests = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=1)
