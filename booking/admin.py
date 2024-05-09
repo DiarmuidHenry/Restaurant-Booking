7f1a44f106313abb4c1ecff77126d0272092208b
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-09 07:19:37 +0000

    refact: More attributes in Reservation

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import RestaurantTable
+from .models import Reservation
 
 # Register your models here.
 admin.site.register(RestaurantTable)
+admin.site.register(Reservation)
--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -11,12 +12,23 @@ class RestaurantTable(models.Model):
 class Reservation(models.Model):
     STATUS_CHOICES = [
         (1, 'Confirmed'),
-        (0, 'Canceled'),
+        (0, 'Cancelled'),
+    ]
+
+    LENGTH_CHOICES = [
+        (1, '1 hour'),
+        (1.5, '1.5 hours'),
+        (2, '2 hours'),
+        (2.5, '2.5 hours'),
+        (3, '3 hours'),
     ]
 
     reservation_id = models.AutoField(primary_key=True)
     customer_name = models.CharField(max_length=100)
     table = models.ForeignKey(RestaurantTable, on_delete=models.CASCADE)
-    date_and_time = models.DateTimeField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
+    reservation_date = models.DateField(default=timezone.now)
+    reservation_time = models.TimeField(default=timezone.now)
+    reservation_length = models.CharField(choices=LENGTH_CHOICES, default=2)
     number_of_guests = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=1)
+    status = models.CharField(choices=STATUS_CHOICES, default=1)
