d39abffd544bd5fa61fcd6e5883d85cadb395ac6
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-28 12:57:22 +0000

    add: form showing on /reservation

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django import forms
+from django.views.generic.edit import FormView
 from django.http import HttpResponse
+from .models import Reservation
 
-# Create your views here.
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
 
-def my_app(request):
-    return HttpResponse("Hello, World!")
+class ReservationView(FormView):
+    template_name = 'booking/booking_form.html'
+    form_class = ReservationForm
+    success_url = '/thank-you/'
+
+    def form_valid(self, form):
+        print("Form is valid")
+        form.save()
+        return super().form_valid(form)
