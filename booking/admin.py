2aaf20212b1504390f2114b2499010bfd73fbc98
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-28 13:51:57 +0000

    add: summernote to booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import RestaurantTable, Reservation, NormalOpeningHours, ExceptionalOpeningHours
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Reservation)
+class ReservationAdmin(SummernoteModelAdmin):
+
+    list_display = ('reservation_id', 'reservation_date', 'reservation_time')
+    search_fields = ['reservation_id', 'customer_name',]
+    list_filter = ('status', 'created_on', 'reservation_date',)
+    # prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('reservation_id',)
 
 # Register your models here.
 admin.site.register(RestaurantTable)
-admin.site.register(Reservation)
 admin.site.register(NormalOpeningHours)
 admin.site.register(ExceptionalOpeningHours)
