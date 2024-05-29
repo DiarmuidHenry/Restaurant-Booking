9756b522e2e35520667eb9d74ffaefca44360b9d
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-29 11:22:47 +0000

    add: basic path through making a reservation

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.views.generic.edit import FormView
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Reservation
 
@@ -11,9 +12,15 @@ class ReservationForm(forms.ModelForm):
 class ReservationView(FormView):
     template_name = 'booking/booking_form.html'
     form_class = ReservationForm
-    success_url = '/thank-you/'
+    success_url = '/thank_you/'
 
     def form_valid(self, form):
         print("Form is valid")
         form.save()
         return super().form_valid(form)
+
+def thank_you(request):
+    return render(request, 'booking/thank_you.html')
+
+def home(request):
+    return render(request, 'booking/home.html')
