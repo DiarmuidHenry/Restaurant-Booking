9e7883a7222bdc1c86cfd84f5b0a363b23d5472f
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-05-29 11:22:47 +0000

    add: Basic path through making a reservation

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import ReservationView
 
 urlpatterns = [
-    path('reservation/', views.ReservationView.as_view(), name='reservation'),
+    path('', views.home, name='home'), 
+    path('reservation/', ReservationView.as_view(), name='reservation'),
+    path('thank_you/', views.thank_you, name='thank_you'),
 ]
