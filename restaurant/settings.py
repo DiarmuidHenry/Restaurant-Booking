2acae525f856381df68626abd7c79a3a2d5d50ea
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-06-12 11:54:49 +0000

    add: allergens app

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-diarmuidhen-restaurantb-wngi8kmlpst.ws-eu114.gitpod.io', '.herokuapp.com',]
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'booking',
     'django_summernote',
+    'allergens',
 ]
 
 MIDDLEWARE = [
