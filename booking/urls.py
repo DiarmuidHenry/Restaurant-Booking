1b6b5ac6047b91d959b07819a9b19de749fae54f
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-06-19 08:19:19 +0000

    feat: start of fetching opening hours in reservation field

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ReservationView, check_availability, home, thank_you, make_reservation
+from .views import ReservationView, check_availability, home, thank_you, make_reservation, get_opening_hours
 
 urlpatterns = [
     path('reservation/', ReservationView.as_view(), name='reservation'),
     path('check_availability/', check_availability, name='check_availability'),
     path('thank_you/', thank_you, name='thank_you'),
     path('make_reservation/<int:table_id>/', make_reservation, name='make_reservation'),
+    path('reservation/get_opening_hours/', get_opening_hours, name='get_opening_hours'),  # Ensure this line
+
 ]
